79711f676a19273d647e2940846914a3e03e8b6b
Yohei Komori <amadeuslions@gmail.com>
2021-06-11 14:55:04 +0900

    broken our houmepage into smaller components, finished styling our menu item

--- components/menu-item.js
+++ components/menu-item.js
@@ -52,8 +52,7 @@ const MenuItemContainer = styled.div`
   :hover {
     cursor: pointer;
     transform: scale(1.1);
-    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45 ,0.95);
-    }
+    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
   }
 `;
 
